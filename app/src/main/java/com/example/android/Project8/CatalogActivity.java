e101eee888ed8ba51aadd1e2baaaf21461e482e1
loboz85 <loboz85@gmail.com>
2018-07-17 13:16:23 +0200

    update

--- app/src/main/java/com/example/android/Project8/CatalogActivity.java
+++ app/src/main/java/com/example/android/Project8/CatalogActivity.java
@@ -26,7 +26,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.example.android.Project8.data.ProductContract.ProdEntry;
 import com.example.android.Project8.data.ProdDbHelper;
@@ -72,8 +72,6 @@ public class CatalogActivity extends AppCompatActivity {
      * the store database.
      */
     private void displayDatabaseInfo() {
-//        // Create and/or open a database to read from it DO USUNIECIA
-//        SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
         // Define a projection that specifies which columns from the database
         // you will actually use after this query.
@@ -86,15 +84,6 @@ public class CatalogActivity extends AppCompatActivity {
                 ProdEntry.COLUMN_SUPP_PHONE};
         System.out.println("**** ProdEntry.TABLE_NAME" + ProdEntry.TABLE_NAME);
 
-//        // Perform a query on the produckts table
-//        Cursor cursor = db.query(
-//                ProdEntry.TABLE_NAME,   // The table to query
-//                projection,            // The columns to return
-//                null,                  // The columns for the WHERE clause
-//                null,                  // The values for the WHERE clause
-//                null,                  // Don't group the rows
-//                null,                  // Don't filter by row groups
-//                null);                   // The sort order
 
         // Perform a query on the provider using the ContentResolver.
         // Use the {@link PetEntry#CONTENT_URI} to access the pet data.
@@ -107,55 +96,14 @@ public class CatalogActivity extends AppCompatActivity {
                 null,                   // Selection criteria
                 null);                  // The sort order for the returned rows
         System.out.println("**** ProdEntry.CONTENT_URI " + cursor);
-        TextView displayView = (TextView) findViewById(R.id.text_view_product);
 
-        try {
-            // Create a header in the Text View that looks like this:
-            //
-            // The products table contains <number of rows in Cursor> pets.
-            // _id - name - price - quantity - supplier - supplier's phone number
-            //
-            // In the while loop below, iterate through the rows of the cursor and display
-            // the information from each column in this order.
-            displayView.setText("The products table contains " + cursor.getCount() + " products.\n\n");
-            displayView.append(ProdEntry._ID + " - " +
-                    ProdEntry.COLUMN_PROD_NAME + " - " +
-                    ProdEntry.COLUMN_PROD_PRICE + " - " +
-                    ProdEntry.COLUMN_PROD_QUANTITY + " - " +
-                    ProdEntry.COLUMN_PROD_SUPP + " - " +
-                    ProdEntry.COLUMN_SUPP_PHONE + "\n");
+        // Find the ListView which will be populated with the pet data
+        ListView prodListView = (ListView) findViewById(R.id.list);
 
-            // Figure out the index of each column
-            int idColumnIndex = cursor.getColumnIndex(ProdEntry._ID);
-            int nameColumnIndex = cursor.getColumnIndex(ProdEntry.COLUMN_PROD_NAME);
-            int priceColumnIndex = cursor.getColumnIndex(ProdEntry.COLUMN_PROD_PRICE);
-            int quantityColumnIndex = cursor.getColumnIndex(ProdEntry.COLUMN_PROD_QUANTITY);
-            int suppColumnIndex = cursor.getColumnIndex(ProdEntry.COLUMN_PROD_SUPP);
-            int phoneColumnIndex = cursor.getColumnIndex(ProdEntry.COLUMN_SUPP_PHONE);
-
-            // Iterate through all the returned rows in the cursor
-            while (cursor.moveToNext()) {
-                // Use that index to extract the String or Int value of the word
-                // at the current row the cursor is on.
-                int currentID = cursor.getInt(idColumnIndex);
-                String currentName = cursor.getString(nameColumnIndex);
-                int currentPrice = cursor.getInt(priceColumnIndex);
-                int currentQuantity = cursor.getInt(quantityColumnIndex);
-                int currentSupp = cursor.getInt(suppColumnIndex);
-                int currentPhone = cursor.getInt(phoneColumnIndex);
-                // Display the values from each column of the current row in the cursor in the TextView
-                displayView.append(("\n" + currentID + " - " +
-                        currentName + " - " +
-                        currentPrice + " - " +
-                        currentQuantity + " - " +
-                        currentSupp + " - " +
-                        currentPhone));
-            }
-        } finally {
-            // Always close the cursor when you're done reading from it. This releases all its
-            // resources and makes it invalid.
-            cursor.close();
-        }
+        // Setup an Adapter to create a list item for each row of pet data in the Cursor.
+        ProdCursorAdapter adapter = new ProdCursorAdapter(this, cursor);
+        // Attach the adapter to the ListView.
+        prodListView.setAdapter(adapter);
     }
 
     /**
