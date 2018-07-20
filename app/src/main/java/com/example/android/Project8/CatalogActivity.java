f1a24c9cca0b264bc06b12b59274b01983e65305
loboz85 <loboz85@gmail.com>
2018-07-20 11:34:03 +0200

    update

--- app/src/main/java/com/example/android/Project8/CatalogActivity.java
+++ app/src/main/java/com/example/android/Project8/CatalogActivity.java
@@ -141,6 +141,15 @@ public class CatalogActivity extends AppCompatActivity implements
         Uri newUri = getContentResolver().insert(ProdEntry.CONTENT_URI, values);
     }
 
+    /**
+     * Helper method to delete all pets in the database.
+     */
+    private void deleteAllPets() {
+        int rowsDeleted = getContentResolver().delete(ProdEntry.CONTENT_URI, null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from pet database");
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
@@ -159,7 +168,7 @@ public class CatalogActivity extends AppCompatActivity implements
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
-                // Do nothing for now
+                deleteAllPets();
                 return true;
         }
         return super.onOptionsItemSelected(item);
